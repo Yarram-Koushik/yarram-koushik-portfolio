c9436e73453edcc34e4d2ff762ecb3b2ecde6be4
koushik <yerramkoushik4@gmail.com>
2026-06-08 12:16:20 +0530

    Update education and certifications

--- src/portfolio.js
+++ src/portfolio.js
@@ -207,7 +207,7 @@ const competitiveSites = {
       style: {
         color: "#F79F1B",
       },
-      profileLink: "https://leetcode.com/layman_brother/",
+      profileLink: "https://leetcode.com/u/yerram_koushik1/",
     },
     {
       siteName: "HackerRank",
@@ -215,7 +215,7 @@ const competitiveSites = {
       style: {
         color: "#2EC866",
       },
-      profileLink: "https://www.hackerrank.com/layman_brother",
+      profileLink: "https://www.hackerrank.com/profile/21r21a6665",
     },
     {
       siteName: "Codechef",
@@ -223,7 +223,7 @@ const competitiveSites = {
       style: {
         color: "#5B4638",
       },
-      profileLink: "https://www.codechef.com/users/ashutosh_1919",
+      profileLink: "https://www.codechef.com/users/yerram_koushik",
     },
     {
       siteName: "Codeforces",
@@ -231,7 +231,7 @@ const competitiveSites = {
       style: {
         color: "#1F8ACB",
       },
-      profileLink: "http://codeforces.com/profile/layman_brother",
+      profileLink: "https://codeforces.com/profile/yerram_koushik",
     },
     {
       siteName: "Hackerearth",
@@ -239,46 +239,56 @@ const competitiveSites = {
       style: {
         color: "#323754",
       },
-      profileLink: "https://www.hackerearth.com/@ashutosh391",
-    },
-    {
-      siteName: "Kaggle",
-      iconifyClassname: "simple-icons:kaggle",
-      style: {
-        color: "#20BEFF",
-      },
-      profileLink: "https://www.kaggle.com/laymanbrother",
+      profileLink: "https://www.hackerearth.com/@yerramkoushik4/",
     },
   ],
 };
 
+// const degrees = {
+//   degrees: [
+//     {
+//       title: "Indian Institute of Information Technology Kurnool",
+//       subtitle: "B.Tech. in Computer Engineering",
+//       logo_path: "iiitk_logo.png",
+//       alt_name: "IIITDM Kurnool",
+//       duration: "2016 - 2020",
+//       descriptions: [
+//         "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+//         "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+//         "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+//       ],
+//       website_link: "http://iiitk.ac.in",
+//     },
+//     {
+//       title: "Indiana University Bloomington",
+//       subtitle: "M.S. in Computer Science",
+//       logo_path: "iu_logo.png",
+//       alt_name: "Indiana University Bloomington",
+//       duration: "2021 - 2023",
+//       descriptions: [
+//         "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
+//         "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
+//         "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
+//       ],
+//       website_link: "https://www.indiana.edu/",
+//     },
+//   ],
+// };
 const degrees = {
   degrees: [
     {
-      title: "Indian Institute of Information Technology Kurnool",
-      subtitle: "B.Tech. in Computer Engineering",
-      logo_path: "iiitk_logo.png",
-      alt_name: "IIITDM Kurnool",
-      duration: "2016 - 2020",
+      title: "MLR Institute of Technology",
+      subtitle:
+        "B.Tech in Computer Science and Engineering (Artificial Intelligence and Machine Learning)",
+      logo_path: "mlrit_logo.png", // Replace with your actual college logo file name if needed
+      alt_name: "MLRIT",
+      duration: "2021 - 2025",
       descriptions: [
-        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ Maintained a strong academic record, achieving an overall CGPA of 8.49 throughout the engineering program.",
+        "⚡ Completed specialized coursework bridging core computer science fields with advanced AI and Machine Learning practices.",
+        "⚡ Built a comprehensive technical foundation covering Data Structures & Algorithms, Object-Oriented Programming, and Database Management.",
       ],
-      website_link: "http://iiitk.ac.in",
-    },
-    {
-      title: "Indiana University Bloomington",
-      subtitle: "M.S. in Computer Science",
-      logo_path: "iu_logo.png",
-      alt_name: "Indiana University Bloomington",
-      duration: "2021 - 2023",
-      descriptions: [
-        "⚡ I have taken varity of courses related to Artificial Intelligence which correspond to Explainable AI, Graph Machine Learning, Computer Vision etc.",
-        "⚡ Apart from this, I have also done research assistantship. As part of it, I have worked on creating new algorithms in Graph ML and Network Science.",
-        "⚡ During my time at university, I was also associated with multimedia department. As part of it, I have worked on some documentry films and interviews.",
-      ],
-      website_link: "https://www.indiana.edu/",
+      website_link: "https://mlrit.ac.in/",
     },
   ],
 };
@@ -286,115 +296,183 @@ const degrees = {
 const certifications = {
   certifications: [
     {
-      title: "Machine Learning",
-      subtitle: "- Andrew Ng",
-      logo_path: "stanford_logo.png",
+      title: "Introduction to MongoDB for Students",
+      subtitle: "- MongoDB",
+      logo_path: "mongodb_logo.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "Stanford University",
-      color_code: "#8C151599",
+        "https://drive.google.com/file/d/1DxFClw-Law8xZAF0G1j7mjSXjQO5xF4h/view?usp=sharing",
+      alt_name: "MongoDB",
+      color_code: "#47A24899",
     },
     {
-      title: "Deep Learning",
-      subtitle: "- Andrew Ng",
-      logo_path: "deeplearning_ai_logo.png",
+      title: "PCAP: Programming Essentials in Python",
+      subtitle: "- Cisco Networking Academy & OpenEDG",
+      logo_path: "cisco_logo1.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
-      alt_name: "deeplearning.ai",
-      color_code: "#00000099",
+        "https://drive.google.com/file/d/13E3PdU1CMPm-raoC3UnK3WLly4Agni1Q/view?usp=sharing",
+      alt_name: "Python Institute",
+      color_code: "#049CA499",
     },
     {
-      title: "ML on GCP",
-      subtitle: "- GCP Training",
-      logo_path: "google_logo.png",
+      title: "SQL (Advanced)",
+      subtitle: "- HackerRank",
+      logo_path: "hackerrank_logo.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
-      alt_name: "Google",
-      color_code: "#0C9D5899",
+        "https://drive.google.com/file/d/1_tdMkH8fM86IU01IwDdh4V8XKfxfao18/view?usp=sharing",
+      alt_name: "HackerRank",
+      color_code: "#2EC86699",
     },
     {
-      title: "Data Science",
-      subtitle: "- Alex Aklson",
-      logo_path: "ibm_logo.png",
+      title: "SQL (Basic)",
+      subtitle: "- HackerRank",
+      logo_path: "hackerrank_logo.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
-      alt_name: "IBM",
-      color_code: "#1F70C199",
+        "https://drive.google.com/file/d/1sBbPT7WkoQqv4ZHdROOu3PVUvBcNOov6/view?usp=sharing",
+      alt_name: "HackerRank",
+      color_code: "#2EC86699",
     },
     {
-      title: "Big Data",
-      subtitle: "- Kim Akers",
-      logo_path: "microsoft_logo.png",
+      title: "Python (Basic)",
+      subtitle: "- HackerRank",
+      logo_path: "hackerrank_logo.png",
       certificate_link:
-        "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
-      alt_name: "Microsoft",
-      color_code: "#D83B0199",
+        "https://drive.google.com/file/d/1HddbZ1dZvRT_wRQv9XfNYKvtOTf3MwaS/view?usp=sharing",
+      alt_name: "HackerRank",
+      color_code: "#2EC86699",
     },
     {
-      title: "Advanced Data Science",
-      subtitle: "- Romeo Kienzler",
-      logo_path: "ibm_logo.png",
+      title: "Java (Basic)",
+      subtitle: "- HackerRank",
+      logo_path: "hackerrank_logo.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
-      alt_name: "IBM",
-      color_code: "#1F70C199",
+        "https://drive.google.com/file/d/1eAb_GPI3s3KiCLDuXbwUnkPitF51gpiU/view?usp=sharing",
+      alt_name: "HackerRank",
+      color_code: "#2EC86699",
     },
     {
-      title: "Advanced ML on GCP",
-      subtitle: "- GCP Training",
-      logo_path: "google_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
-      alt_name: "Google",
-      color_code: "#0C9D5899",
-    },
-    {
-      title: "DL on Tensorflow",
-      subtitle: "- Laurence Moroney",
-      logo_path: "deeplearning_ai_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
-      alt_name: "deeplearning.ai",
-      color_code: "#00000099",
-    },
-    {
-      title: "Fullstack Development",
-      subtitle: "- Jogesh Muppala",
-      logo_path: "coursera_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
-      alt_name: "Coursera",
-      color_code: "#2A73CC",
-    },
-    {
-      title: "Kuberenetes on GCP",
-      subtitle: "- Qwiklabs",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
-      alt_name: "GCP",
-      color_code: "#4285F499",
-    },
-    {
-      title: "Cryptography",
-      subtitle: "- Saurabh Mukhopadhyay",
+      title: "Programming, Data Structures and Algorithms Using Python",
+      subtitle: "- NPTEL",
       logo_path: "nptel_logo.png",
       certificate_link:
-        "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+        "https://drive.google.com/file/d/1tq8y4CGHMa-c1OyZFVkKaSoOkA4xOxzh/view?usp=sharing",
       alt_name: "NPTEL",
-      color_code: "#FFBB0099",
-    },
-    {
-      title: "Cloud Architecture",
-      subtitle: "- Qwiklabs",
-      logo_path: "gcp_logo.png",
-      certificate_link:
-        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
-      alt_name: "GCP",
-      color_code: "#4285F499",
+      color_code: "#FF990099",
     },
   ],
 };
+
+// const certifications = {
+//   certifications: [
+//     {
+//       title: "Machine Learning",
+//       subtitle: "- Andrew Ng",
+//       logo_path: "stanford_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "Stanford University",
+//       color_code: "#8C151599",
+//     },
+//     {
+//       title: "Deep Learning",
+//       subtitle: "- Andrew Ng",
+//       logo_path: "deeplearning_ai_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+//       alt_name: "deeplearning.ai",
+//       color_code: "#00000099",
+//     },
+//     {
+//       title: "ML on GCP",
+//       subtitle: "- GCP Training",
+//       logo_path: "google_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+//       alt_name: "Google",
+//       color_code: "#0C9D5899",
+//     },
+//     {
+//       title: "Data Science",
+//       subtitle: "- Alex Aklson",
+//       logo_path: "ibm_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
+//       alt_name: "IBM",
+//       color_code: "#1F70C199",
+//     },
+//     {
+//       title: "Big Data",
+//       subtitle: "- Kim Akers",
+//       logo_path: "microsoft_logo.png",
+//       certificate_link:
+//         "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
+//       alt_name: "Microsoft",
+//       color_code: "#D83B0199",
+//     },
+//     {
+//       title: "Advanced Data Science",
+//       subtitle: "- Romeo Kienzler",
+//       logo_path: "ibm_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+//       alt_name: "IBM",
+//       color_code: "#1F70C199",
+//     },
+//     {
+//       title: "Advanced ML on GCP",
+//       subtitle: "- GCP Training",
+//       logo_path: "google_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
+//       alt_name: "Google",
+//       color_code: "#0C9D5899",
+//     },
+//     {
+//       title: "DL on Tensorflow",
+//       subtitle: "- Laurence Moroney",
+//       logo_path: "deeplearning_ai_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
+//       alt_name: "deeplearning.ai",
+//       color_code: "#00000099",
+//     },
+//     {
+//       title: "Fullstack Development",
+//       subtitle: "- Jogesh Muppala",
+//       logo_path: "coursera_logo.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
+//       alt_name: "Coursera",
+//       color_code: "#2A73CC",
+//     },
+//     {
+//       title: "Kuberenetes on GCP",
+//       subtitle: "- Qwiklabs",
+//       logo_path: "gcp_logo.png",
+//       certificate_link:
+//         "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+//       alt_name: "GCP",
+//       color_code: "#4285F499",
+//     },
+//     {
+//       title: "Cryptography",
+//       subtitle: "- Saurabh Mukhopadhyay",
+//       logo_path: "nptel_logo.png",
+//       certificate_link:
+//         "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+//       alt_name: "NPTEL",
+//       color_code: "#FFBB0099",
+//     },
+//     {
+//       title: "Cloud Architecture",
+//       subtitle: "- Qwiklabs",
+//       logo_path: "gcp_logo.png",
+//       certificate_link:
+//         "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+//       alt_name: "GCP",
+//       color_code: "#4285F499",
+//     },
+//   ],
+// };
 
 // Experience Page
 const experience = {
